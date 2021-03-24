297b9de3cede4d6b0fe4d493f2222b0a300c978b
Twan <twanwalpot@gmail.com>
2021-03-24 20:25:33 +0100

    added setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ from setuptools import setup
 setup(
     name="Esat",
     author="Nassim",
-    install_requires=["opencv-python==4.5.1.48", "matplotlib==3.3.4"],
+    install_requires=[
+        "opencv-python==4.5.1.48",
+        "matplotlib==3.3.4",
+        "xlsxwriter==1.3.7",
+    ],
 )
