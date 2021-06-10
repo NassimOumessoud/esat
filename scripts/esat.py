ab90d20b00f067a0342c46a4a8ce7b0266638172
Nassim <nassim.oumes@gmail.com>
2021-06-10 12:43:50 +0200

    Radius size cap and circle detection params for accuracy

--- scripts/esat.py
+++ scripts/esat.py
@@ -10,9 +10,13 @@ def pixels_to_micrometers(pixels, image):
     """
     if image == 800:
         c = 3
+        well_size = 333
     else:
         c = 1.6
-    return pixels / c  # aantal micrometer
+        well_size = 177
+    micrometers = pixels/c
+    
+    return micrometers, well_size
 
 
 def circular_area(radius):
@@ -89,17 +93,28 @@ def files(route, img_path):
     for dirpath, _, files in os.walk(route):
         file_counter = 0
         radius = 0
+        well = 0
         files.sort()
+        
         for file_name in files:
             file_path = os.path.join(dirpath, file_name)
-            mid = len(files) / 2
             image = cv2.imread(file_path, 0)  
-            min_rad = int(round(radius * 0.8))
-            max_rad = 180
+            
+            if radius >= well:
+                min_rad = 10
+                max_rad = int(round(well * 0.9))
+            else:
+                min_rad = int(round(radius * 0.8))
+                max_rad = int(round(radius * 1.5))
+                
             result = detect_circle(image, min_rad, max_rad)
+            
+            
             if result:
-                radius, center, size, img = result
-                surfaces.append(size)
+                radius, center, img = result
+                mm_radius, well = pixels_to_micrometers(radius, len(img))
+                area = round(circular_area(mm_radius))
+                surfaces.append(area)
                 
                 img_file = os.path.join(img_path, str(file_counter))
                 cv2.imwrite(img_file + ".png", img)
@@ -109,13 +124,12 @@ def files(route, img_path):
     return surfaces
 
 
-def detect_circle(img, min_radius, max_radius, p1=200, p2=100):
+def detect_circle(img, min_radius, max_radius, p1=160, p2=100):
     """
     Detects circles with a given min and max radius in an image. Param1 and param2
     refer to the canny edge detection that the HoughCircles uses to detect the
     circles.
     """
-    size = len(img)
     for p in range(p1, p2, -10):
 
         detected_circles = cv2.HoughCircles(
@@ -124,7 +138,7 @@ def detect_circle(img, min_radius, max_radius, p1=200, p2=100):
             1.5,
             290,
             param1=p,
-            param2=p / 2,
+            param2=p / 4,
             minRadius=min_radius,
             maxRadius=max_radius)
         
@@ -133,8 +147,7 @@ def detect_circle(img, min_radius, max_radius, p1=200, p2=100):
             detected_circles_rounded = np.uint16(np.around(detected_circles))
             for i in detected_circles_rounded[0, :]:
                     cv2.circle(img, (i[0], i[1]), i[2], (255), 5)  # Teken cirkel
-                    radius = pixels_to_micrometers(i[2], size)
-                    area = round(circular_area(radius))
-                    return radius, (i[0], i[1]), area, img
+                    radius = i[2]
+                    return radius, (i[0], i[1]), img
         else:
             continue
